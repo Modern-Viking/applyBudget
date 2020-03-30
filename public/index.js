a7303508a29ba45966ec3b6cdb462d0c63efe3b9
Modern Viking <thomascustoms715@gmail.com>
2020-03-30 15:40:01 -0600

    final bug fixes

--- public/index.js
+++ public/index.js
@@ -117,7 +117,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
-    saveRecord(transaction);
+    save(transaction);
     // clear form
     nameEl.value = "";
     amountEl.value = "";
